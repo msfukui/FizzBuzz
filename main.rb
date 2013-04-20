abac1a5e49f9dd63de2fa1509d47a60c4d0051d6
Fukui Masayuki <m-fukui@biglobe.co.jp>
2013-04-21 01:00:11 +0900

    main 処理にコメントを追加。結果の整形箇所で改行の出力部分にバグがあったので修正。

--- main.rb
+++ main.rb
@@ -5,18 +5,23 @@ $LOAD_PATH.unshift(lib_dir)
 
 require 'FizzBuzz'
 
-print "最初と最後の数字を入力して Enter キーを押してください。[1-100]: "
+print "FizzBuzz したい開始と終了の数値を入力して Enter キーを押してください。[1-100]: "
 number = gets.chomp.split(/\-/)
 
+# 何も入力されなかった時の初期値。
 if number == [] then
   number = [ 1, 100 ]
 end
+# 形式間違ってた場合。
 unless number.size == 2 then
   puts "* 入力エラーです。(" << number.join(',') << ")"
   puts "  プログラムを終了します。"
   exit 1
 end
 
+# 入力値が数値以外だった場合の例外処理受け取りのため、
+# to_i() ではなく Integer() を使用。
+# 例外の日本語化はちょっと面倒なのでいったんあきらめ。
 begin
   start_number = Integer(number[0])
   end_number   = Integer(number[1])
@@ -27,12 +32,19 @@ rescue => exception
   puts "  プログラムを終了します。"
   exit 2
 else
+	# 計算結果を整形して出力。
+	# 3 桁以上の数値の整形はちょっと面倒なのでいったんあきらめ。
   puts "結果:"
   out.exec.each_with_index { |item,index|
-    printf "%3d: %-9s,", index+1, item
+    printf "%3d: %-9s ", index+start_number, item
+		# 5 個毎に改行。
     if (index+1) % 5 == 0 then
       print "\n"
     end
   }
+	# 表示数が 5 で割り切れない時は最後に改行を追加。
+  unless (end_number-start_number+1) % 5 == 0 then
+    print "\n"
+  end
   exit 0
 end
